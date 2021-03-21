ae93c493fe3f3bb7e3e503c74f1744320397de20
kyleschmitz1 <kylehudson125@msn.com>
2021-03-21 14:20:15 -0400

    New Build Settings

--- online-journal/src/App.js
+++ online-journal/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Hello, World</h3>
+        <h3>Hello, World New Build</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
